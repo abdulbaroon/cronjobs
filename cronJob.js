94d0739ac3efcae5463bc724cf4d4c49d241a287
devashishHp04 <devashish04.hp@gmail.com>
2024-09-27 16:37:29 +0530

    fixed issue

--- cronJob.js
+++ cronJob.js
@@ -1,7 +1,12 @@
+const express = require('express');
 const cron = require('node-cron');
 const axios = require('axios');
 
-cron.schedule('* * * * * *', async () => {
+const app = express();
+const PORT = 8080;
+
+// Cron job: Runs every minute
+cron.schedule('* * * * *', async () => {
   try {
     console.log('Making API call...');
     const response = await axios.get('https://hp-node-api.onrender.com/');
@@ -11,4 +16,12 @@ cron.schedule('* * * * * *', async () => {
   }
 });
 
-console.log('Cron job started. It will run every minute.');
+// Start the server on port 8080
+app.get('/', (req, res) => {
+  res.send('Server is running along with the cron job!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log('Cron job started. It will run every minute.');
+});
